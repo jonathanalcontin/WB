900d6a08555cf6849f4c32bacad2e8d51dbb1dd2
Jonathan Alcontin <jonathanalcontin@Jonathans-MacBook-Pro.local>
2014-07-16 15:39:34 -0400

    Damage = Money added. Attempting to make store scene. Create cleaner UI. Add cool Win message

---
+++ WB.spritebuilder/Source/GameDataCoin.h
@@ -0,0 +1,13 @@
+//
+//  GameDataCoin.h
+//  WB
+//
+//  Created by Jonathan Alcontin on 7/16/14.
+//  Copyright (c) 2014 Apportable. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface GameDataCoin : NSObject
+
+@end
---
+++ WB.spritebuilder/Source/GameDataCoin.m
@@ -0,0 +1,13 @@
+//
+//  GameDataCoin.m
+//  WB
+//
+//  Created by Jonathan Alcontin on 7/16/14.
+//  Copyright (c) 2014 Apportable. All rights reserved.
+//
+
+#import "GameDataCoin.h"
+
+@implementation GameDataCoin
+
+@end
