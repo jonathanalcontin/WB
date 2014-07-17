159ba65933c373b11424b6a6e72803c5b65e247e
Jonathan Alcontin <jonathanalcontin@Jonathans-MacBook-Pro.local>
2014-07-17 17:39:28 -0400

    store almost implemented

---
+++ WB.spritebuilder/Source/Store.h
@@ -0,0 +1,13 @@
+//
+//  Store.h
+//  WB
+//
+//  Created by Jonathan Alcontin on 7/17/14.
+//  Copyright (c) 2014 Apportable. All rights reserved.
+//
+
+#import "CCNode.h"
+
+@interface Store : CCNode
+
+@end
---
+++ WB.spritebuilder/Source/Store.m
@@ -0,0 +1,13 @@
+//
+//  Store.m
+//  WB
+//
+//  Created by Jonathan Alcontin on 7/17/14.
+//  Copyright (c) 2014 Apportable. All rights reserved.
+//
+
+#import "Store.h"
+
+@implementation Store
+
+@end
