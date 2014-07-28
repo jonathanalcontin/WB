889ebd80161614505d89a3da1dab7659e204114f
Jonathan Alcontin <jonathanalcontin@Jonathans-MacBook-Pro.local>
2014-07-28 13:55:07 -0400

    Robot now is animated when shooting. Working on wall changing sprites as it gets decreases in HP

---
+++ WB.spritebuilder/Mechshoot.m
@@ -0,0 +1,13 @@
+//
+//  Mechshoot.m
+//  WB
+//
+//  Created by Jonathan Alcontin on 7/28/14.
+//  Copyright (c) 2014 Apportable. All rights reserved.
+//
+
+#import "Mechshoot.h"
+
+@implementation Mechshoot
+
+@end
--- WB.spritebuilder/Source/Gameplay.m
+++ WB.spritebuilder/Source/Gameplay.m
@@ -11,10 +11,12 @@
 #import "Wall.h"
 #import "GameDataCoin.h"
 #import "Store.h"
+#import "Mechshoot.h"
 
 @implementation Gameplay {
     CCPhysicsNode *_physicsNode;
     CCNode *_mech1;
+    Mechshoot *_mech2;
     CCNode *_levelNode;
     CCNode *_health;
     CCLabelTTF *_scoreLabel;
@@ -23,28 +25,50 @@
 }
 // is called when CCB file has completed loading
 - (void)didLoadFromCCB {
+    
+    
+    //loads mecha with timelines
+    _mech2 = (Mechshoot *) [CCBReader load:@"mech2" owner:self];
+    _mech2.position = ccp(35, 68.1);
+    [self addChild: _mech2];
     // tell this scene to accept touches
     self.userInteractionEnabled = TRUE;
     CCNode *level = [CCBReader load:@"Levels/Level1"];
     [_levelNode addChild:level];
     _physicsNode.collisionDelegate = self;
     _life = 100;
-//    [_bullet addObserver:self forKeyPath:@"score" options:0 context:NULL];
+    
+    
     
 }
 
 // called on every touch in this scene
 - (void)touchBegan:(UITouch *)touch withEvent:(UIEvent *)event {
-  [self launchbullet];
+//    [self launchbullet];
+    [[_mech2 animationManager] runAnimationsForSequenceNamed: @"Shoot time"];
 }
 
-- (void)launchbullet {
+//- (void)touchEnded:(UITouch *)touch withEvent:(UIEvent *)event {
+//    
+//    [[_mech2 animationManager] runAnimationsForSequenceNamed: @"what time"];
+//    use this as a base to create a laser charge animation
+//    
+//}
+
+- (void)launchBullet {
+ 
+    //calling launchBullet method is also used as a selector in Spritebuilder in Mech2.ccb to call "Shoot time"
+    
+    //---------------------bullet code ------------------
+    
    // loads the bullet.ccb we have set up in Spritebuilder
    Bullet* bullet = (Bullet*)[CCBReader load:@"bullet"];
     // position the bullets at the front of the gun of mecha
-   bullet.position = ccpAdd(_mech1.position, ccp(100, 130));
+   bullet.position = ccpAdd(_mech2.position, ccp(100, 65));
 // add the bullet to the physicsNode of this scene (because it has physics enabled)
 
+    
+    
      [_physicsNode addChild:bullet];
   
 //manually create & apply a force to launch the bullet
@@ -56,7 +80,7 @@
 - (void)ccPhysicsCollisionPostSolve:(CCPhysicsCollisionPair *)pair bullet:(CCNode *)bullet wall:(CCNode *)wall {
     // collision handling
     [self hit:wall];
-    [GameDataCoin sharedData].coins += ((Bullet*)bullet).myCustomIsaac; //(i want to pull the value from the custom property set in bullet)+ armyunit.value;
+    [GameDataCoin sharedData].coins += ((Bullet*)bullet).myCustomIsaac; //(i want to pull the value from the custom property myCustomIsaac set in bullet)+ armyunit.value, I probably want the damage to incrementally get stronger each time an army unit is purchased (and change the default sprite based on how large this integer becomes;
     [bullet removeFromParent];
     CCLOG(@"Bullet hit the wall finnally!");
     
@@ -83,6 +107,7 @@
 }
 - (void)endGameWithMessage:(NSString*)message {
     CCLOG(@"%@",message);
+    
  
 }
 
